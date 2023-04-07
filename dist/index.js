3a5eed85933e5631eeeac2e02da0f975a723bb59
Siraj Alam <sirajalam049@gmail.com>
2023-04-07 11:49:33 +0530

    value prop is passed to date component

--- dist/index.js
+++ dist/index.js
@@ -305,7 +305,7 @@ var MUIDatePicker = function (props) {
         var dateValue = (outputFormat === 'date') ? date : date.format(outputFormat || fieldProps.format || 'MM/DD/YYYY');
         formikProps.setFieldValue(fieldProps.name, dateValue, false);
     };
-    var updatedProps = __assign(__assign({}, datePickerProps), { error: !!fieldError, helperText: (fieldError || ''), onChange: handleDateChange, value: (!value) ? null : undefined, inputValue: (!value) ? '' : value, format: fieldProps.format || 'MM/DD/YYYY', onError: function (error) {
+    var updatedProps = __assign(__assign({}, datePickerProps), { error: !!fieldError, helperText: (fieldError || ''), onChange: handleDateChange, value: (!value) ? null : value, inputValue: (!value) ? '' : value, format: fieldProps.format || 'MM/DD/YYYY', onError: function (error) {
             // handle as a side effect
             if (error !== fieldError) {
                 formikProps.setFieldError(fieldProps.name, error);
--- src/lib/ml-form-builder/lib/MUIDateTimePicker.tsx
+++ src/lib/ml-form-builder/lib/MUIDateTimePicker.tsx
@@ -29,7 +29,7 @@ export const MUIDatePicker: React.FC<IFieldProps & { fieldProps?: IMUIDatePicker
         error: !!fieldError,
         helperText: (fieldError || ''),
         onChange: handleDateChange,
-        value: (!value) ? null : undefined,
+        value: (!value) ? null : value,
         inputValue: (!value) ? '' : value,
         format: fieldProps.format || 'MM/DD/YYYY',
 
